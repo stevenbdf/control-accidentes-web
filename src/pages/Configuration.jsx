d7526a7eedce36470eac2d5a3830eff8ffba3e99
Steven Diaz <stevenbdf@gmail.com>
2021-03-18 22:46:46 -0600

    ✨ Integrate configuration

--- src/pages/Configuration.jsx
+++ src/pages/Configuration.jsx
@@ -3,15 +3,22 @@ import { useDispatch, useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { faSave } from '@fortawesome/free-solid-svg-icons';
+import { useForm } from 'react-hook-form';
 import Modal from '../components/Modal';
 import Switch from '../components/Switch';
 import { show, update } from '../store/config/actions';
+import validator from '../helpers/validator';
 
 const Config = () => {
   const dispatch = useDispatch();
   const isLoading = useSelector((state) => state.config.isLoading);
   const config = useSelector((state) => state.config.config);
   const [open, setOpen] = useState(false);
+  const {
+    register, handleSubmit, errors,
+  } = useForm({
+    mode: 'onChange',
+  });
 
   const [checks, setChecks] = useState({
     checkedA: true,
@@ -19,7 +26,6 @@ const Config = () => {
     checkedC: true,
   });
 
-  console.log({ isLoading, config });
   useEffect(() => {
     dispatch(show({ id: 1 }));
   }, []);
@@ -41,6 +47,15 @@ const Config = () => {
         display_main_info: checks.checkedA,
         display_media: checks.checkedB,
         display_charts: checks.checkedC,
+      },
+    }));
+  };
+
+  const onSubmit = (data) => {
+    dispatch(update({
+      id: 1,
+      body: {
+        ...data,
       },
     }));
   };
@@ -107,53 +122,76 @@ const Config = () => {
           </button>
         </div>
       </div>
-      <div className="border-2 border-blue-400 mt-16 space-y-8 rounded-lg md:flex md:flex-col mb-12">
+      <form onSubmit={handleSubmit(onSubmit)} className="border-2 border-blue-400 mt-16 space-y-8 rounded-lg md:flex md:flex-col mb-12">
         <div className="text-2xl pl-12 pt-4 font-bold">
           Informacion general
         </div>
-        <div className="flex items-center pl-12">
-          <div className="mr-6 text-lg">Fecha ultimo accidente:</div>
-          <input className="border-4 px-4 py-2 border-blue-600 rounded-lg" type="date" />
-        </div>
-        <div className="xl:flex md:space-y-5 md:px-12 lg:px-12 xl:items-center lg:flex-wrap">
-          <div className="flex items-center ">
-            <div className="text-lg lg:mr-6 2xl:mr-16">Texto informativo:</div>
-            <textarea
-              placeholder="Escriba aqui su texto informativo"
-              className="xl:-ml-14 xl:mr-10 border-2 px-4 py-2 border-blue-600 rounded-lg"
-              name=""
-              id=""
-              cols="70"
-              rows="4"
-            />
-          </div>
-          <div className="flex items-center">
-            <div className="text-lg lg:mr-6 2xl:mr-16 2xl:mb-8">Texto marquesina:</div>
-            <textarea
-              placeholder="Escriba aqui su texto marquesina"
-              className="xl:-ml-14 2xl:mb-6  border-2 px-4 py-2 border-blue-600 rounded-lg"
-              name=""
-              id=""
-              cols="70"
-              rows="4"
-            />
-          </div>
-        </div>
-        <div className="flex justify-end mr-4">
-          <button
-            type="button"
-            className="px-4 py-2 bg-eastern-blue-500 rounded-lg mb-4 focus:outline-none text-white hover:text-black hover:bg-blue-400 transition-colors duration-300 "
-            onClick={() => {
-              setOpen(true);
-            }}
-          >
-            <FontAwesomeIcon icon={faSave} className="mr-2" />
-            {' '}
-            Guardar
-          </button>
-        </div>
-
-      </div>
+        {
+          isLoading
+            ? (
+              <div className="w-full flex justify-center items-center h-64">
+                <CircularProgress />
+              </div>
+            )
+            : (
+              <>
+                <div className="flex items-center pl-12">
+                  <div className="mr-6 text-lg">Fecha ultimo accidente:</div>
+                  <input
+                    name="last_accident"
+                    defaultValue={config.last_accident}
+                    ref={register({ required: validator.required })}
+                    className="border-2 px-4 py-2 border-blue-600 rounded-lg"
+                    type="date"
+                  />
+                  {errors.last_accident && <p className="my-1 text-red-500">{errors.last_accident.message}</p>}
+                </div>
+                <div className="xl:flex md:space-y-5 md:px-12 lg:px-12 xl:items-center lg:flex-wrap">
+                  <div className="flex items-center ">
+                    <div className="text-lg lg:mr-6 2xl:mr-16">Texto informativo:</div>
+                    <div>
+                      <textarea
+                        ref={register({ required: validator.required })}
+                        defaultValue={config.main_info_text}
+                        placeholder="Escriba aqui su texto informativo"
+                        className="xl:-ml-14 xl:mr-10 border-2 px-4 py-2 border-blue-600 rounded-lg"
+                        name="main_info_text"
+                        cols="70"
+                        rows="4"
+                      />
+                      {errors.main_info_text && <p className="w-full my-1 xl:-ml-14 text-red-500">{errors.main_info_text.message}</p>}
+                    </div>
+                  </div>
+                  <div className="flex items-center">
+                    <div className="text-lg lg:mr-6 2xl:mr-16 2xl:mb-8">Texto marquesina:</div>
+                    <div>
+                      <textarea
+                        ref={register({ required: validator.required })}
+                        defaultValue={config.marquee_text}
+                        placeholder="Escriba aqui su texto marquesina"
+                        className="xl:-ml-14 2xl:mb-6  border-2 px-4 py-2 border-blue-600 rounded-lg"
+                        name="marquee_text"
+                        cols="70"
+                        rows="4"
+                      />
+                      {errors.marquee_text && <p className="w-full my-1 xl:-ml-14 text-red-500">{errors.marquee_text.message}</p>}
+                    </div>
+                  </div>
+                </div>
+                <div className="flex justify-end mr-4">
+                  <button
+                    type="submit"
+                    className="px-4 py-2 bg-eastern-blue-500 rounded-lg mb-4 focus:outline-none text-white hover:text-black hover:bg-blue-400 transition-colors duration-300 "
+                  >
+                    <FontAwesomeIcon icon={faSave} className="mr-2" />
+                    {' '}
+                    Guardar
+                  </button>
+                </div>
+              </>
+            )
+        }
+      </form>
     </div>
   );
 };
